ba9a62e7c558aacecc77ec5f58087ec5d1d63a36
srpvpn <srpvpn@users.noreply.github.com>
2026-02-03 23:59:44 +0300

    Initial commit

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -4,10 +4,10 @@ export async function GET() {
   const config = {
     accountAssociation: {
       header:
-        "",
-      payload: "",
+        "eyJmaWQiOjI1MjEzNjAsInR5cGUiOiJhdXRoIiwia2V5IjoiMHgzQTA0MUM0RTdhMDk4NGNDMGYxMTZlMzhiRTI4Y0E4RTg4Njk1OTQzIn0",
+      payload: "eyJkb21haW4iOiJmcmFtZXMtdjItZGVtby1ldGEudmVyY2VsLmFwcCJ9",
       signature:
-        "",
+        "zsP6oSBbVNzqELfyRkweSWZZ6KYFD9OR7bMPjCA4FwdAaAvUscOLLM0LAUoVw1cgYOyiooKRlXnqKPorld1ZwRw",
     },
       "frame": {
         "version": "1",
